6a6367c1050bb0dfa37c27ff7a0462384548bed0
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2015-08-24 01:41:27 +0200

    First controller

--- js/flatlander.js
+++ js/flatlander.js
@@ -1,1 +1,11 @@
-var app = angular.module('store', []);
+(function(){
+    var app = angular.module('store', []);
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.89,
+        description: 'Lorem ipsum, lorem ipsum...'
+    };
+})();
