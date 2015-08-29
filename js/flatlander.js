3c1207ed407858238c007a7bdc45d9dd742b8129
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2015-08-29 23:20:35 +0200

    Services ($http) and dependency injection

--- js/flatlander.js
+++ js/flatlander.js
@@ -1,8 +1,12 @@
 (function(){
     var app = angular.module('store', ['store-products']);
-    app.controller('StoreController', function(){
-        this.products = gems;
-    });
+    app.controller('StoreController', ['$http', function($http){
+        var store = this;
+        store.products = [];
+        $http.get('products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
     app.controller('GalleryController', function(){
         this.current = 0;
 
@@ -18,113 +22,4 @@
             this.review = {}
         };
     });
-   var gems = [
-    {
-        name: 'Dodecahedron',
-        price: 2,
-        description: 'Lorem ipsum, lorem ipsum...',
-        canPurchase: true,
-        soldOut: false,
-        images: [
-        {
-            full: "img/gem-1-full.png",
-            thumb: "img/gem-1-thumb.png"
-        },
-        {
-            full: "img/gem-2-full.png",
-            thumb: "img/gem-2-thumb.png"
-        },
-        {
-            full: "img/gem-3-full.png",
-            thumb: "img/gem-3-thumb.png"
-        }],
-        reviews: [
-        {
-            stars: 4,
-            body: "first comment",
-            author: "annonymous"
-        },
-        {
-            stars: 2,
-            body: "second comment",
-            author: "annonymous2"
-        },
-        {
-            stars: 5,
-            body: "third comment",
-            author: "annonymous3"
-        }]
-    },
-    {
-        name: 'Hexagon gem',
-        price: 2.89,
-        description: 'Lorem ipsum, lorem ipsum...',
-        canPurchase: true,
-        soldOut: false,
-        images: [
-        {
-            full: "img/gem-1-full.png",
-            thumb: "img/gem-1-thumb.png"
-        },
-        {
-            full: "img/gem-2-full.png",
-            thumb: "img/gem-2-thumb.png"
-        },
-        {
-            full: "img/gem-3-full.png",
-            thumb: "img/gem-3-thumb.png"
-        }],
-        reviews: [
-        {
-            stars: 4,
-            body: "first comment",
-            author: "annonymous"
-        },
-        {
-            stars: 2,
-            body: "second comment",
-            author: "annonymous2"
-        },
-        {
-            stars: 5,
-            body: "third comment",
-            author: "annonymous3"
-        }]
-    },
-    {
-        name: 'Square gem',
-        price: 2.09,
-        description: 'Lorem ipsum, lorem ipsum...',
-        canPurchase: true,
-        soldOut: false,
-        images: [
-        {
-            full: "img/gem-1-full.png",
-            thumb: "img/gem-1-thumb.png"
-        },
-        {
-            full: "img/gem-2-full.png",
-            thumb: "img/gem-2-thumb.png"
-        },
-        {
-            full: "img/gem-3-full.png",
-            thumb: "img/gem-3-thumb.png"
-        }],
-        reviews: [
-        {
-            stars: 4,
-            body: "first comment",
-            author: "annonymous"
-        },
-        {
-            stars: 2,
-            body: "second comment",
-            author: "annonymous2"
-        },
-        {
-            stars: 5,
-            body: "third comment",
-            author: "annonymous3"
-        }]
-    }];
 })();
